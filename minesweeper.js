ced0a2d1e06e9fa662def012906888a48ac8e358
Tiago Zanarde <zanarde.tiago@gmail.com>
2023-07-27 23:51:45 -0300

    js checkpoint

--- minesweeper.js
+++ minesweeper.js
@@ -1,112 +1,81 @@
 var minesweeper = [];
+const xElements = 10;
+const yElements = 10;
+const bombChances = 20;
 
-function hideFloor(floor) {
-    floor.style.display = 'none';
-    var left = (Number(floor.style.left.replace('%', '')) - 5) / 10
-    var top = (Number(floor.style.top.replace('%', '')) - 5) / 10
-    cave = document.getElementById('cave:' + left + '/' + top);
-    var isBlank = null;
-    var count = 1;
-    if (cave.classList.contains('blank')) {
-
-        // Up
-        do {
-            current_floor = document.getElementById('floor:' + left + '/' + (top - count));
-            current_cave = document.getElementById('cave:' + left + '/' + (top - count));
-            if ((top - count) >= 0) {
-                count++;
-                isBlank = true;
-                current_floor.style.display = 'none';
-            } else if (current_cave.classList.contains('number')) {
-                count = 0;
-                isBlank = false;
-                current_floor.style.display = 'none';
+function defineElements(xAxis, yAxis, bombChances) {
+    for (let x = 0; x < xAxis; x++) {
+        for (let y = 0; y < yAxis; y++) {
+            if (Math.random() >= (1 - (1 / ((xAxis * yAxis) / bombChances)))) {
+                minesweeper[[x, y]] = 'B';
             }
-        } while (isBlank);
-
-        isBlank = null;
-        count = 1;
-
-        // Down
-        // do {
-        //     current_floor = document.getElementById('floor:' + left + '/' + (top + count));
-        //     current_cave = document.getElementById('cave:' + left + '/' + (top + count));
-        //     console.log('A');
-        //     if ((top + count) <= 9) {
-        //         count++;
-        //         isBlank = true;
-        //     } else {
-        //         count = 0;
-        //         isBlank = false;
-        //     }
-        //     current_floor.style.display = 'none';
-        // } while (isBlank);
+        }
+    }
+    for (let x = 0; x < xAxis; x++) {
+        for (let y = 0; y < yAxis; y++) {
+            let number = numberOfAdjacentBombs(x, y);
+            if ((number > 0) && (minesweeper[[x, y]] != 'B')) {
+                minesweeper[[x, y]] = String(number);
+            }
+        }
+    }
+    for (let x = 0; x < xAxis; x++) {
+        for (let y = 0; y < yAxis; y++) {
+            if (minesweeper[[x, y]] === undefined) {
+                minesweeper[[x, y]] = '';
+            }
+        }
     }
 }
 
-function toogleFlag(floor) {
-    if (floor.innerHTML == 'F') {
-        floor.innerHTML = '';
-        floor.style.color = 'black';
-        floor.onclick = function() { hideFloor(floor) };
+function positionElements() {
+    for (let x = 0; x < xElements; x++) {
+        for (let y = 0; y < yElements; y++) {
+            newElement(x, y, minesweeper[[x, y]]);
+        }
+    }
+}
+
+function newElement(left, top, value) {
+    const element = document.createElement('div');
+    const content = document.createElement('h3');
+    content.innerHTML = value;
+    if (value == 'B') {
+        element.classList.add('space', 'bomb');
+    } else if (value == '') {
+        element.classList.add('space', 'blank');
     } else {
-        floor.innerHTML = 'F';
-        floor.style.color = 'blue';
-        floor.onclick = function() {};
+        element.classList.add('space', 'number');
     }
-}
-
-function newFloor(left, top) {
-    const floor = document.createElement('div')
-    floor.classList.add('space', 'floor');
-    floor.style.left = ((left * 10) + 5) + '%';
-    floor.style.top = ((top * 10) + 5) + '%';
-    floor.onclick = function() { hideFloor(floor) };
-    floor.oncontextmenu = function() {
-        toogleFlag(floor);
+    element.classList.add('floor');
+    element.onclick = function() { showElement(element) };
+    element.oncontextmenu = function() {
+        toogleFlag(element);
         return false;
     };
-    floor.id = 'floor:' + left + '/' + top;
-    document.getElementById('minefield').appendChild(floor);
+    content.style.display = 'none';
+    element.style.left = ((left * 10) + 5) + '%';
+    element.style.top = ((top * 10) + 5) + '%';
+    if ((value !== null) && (value !== 'undefined')) { element.appendChild(content); };
+    element.id = 'element:' + left + '/' + top;
+    document.getElementById('minefield').appendChild(element);
 }
 
-function newBomb(left, top) {
-    const bomb = document.createElement('div')
-    const mine = document.createElement('h3');
-    mine.innerHTML = 'B';
-    bomb.classList.add('space', 'bomb');
-    bomb.style.left = ((left * 10) + 5) + '%';
-    bomb.style.top = ((top * 10) + 5) + '%';
-    bomb.appendChild(mine);
-    bomb.id = 'cave:' + left + '/' + top;
-    document.getElementById('minefield').appendChild(bomb);
+function showElement(element) {
+    element.classList.remove('floor');
+    if (element.children.length > 0 ) { element.childNodes[0].style.display = 'inline'; };
 }
 
-function newBlank(left, top) {
-    const blank = document.createElement('div')
-    blank.classList.add('space', 'blank');
-    blank.style.left = ((left * 10) + 5) + '%';
-    blank.style.top = ((top * 10) + 5) + '%';
-    blank.id = 'cave:' + left + '/' + top;
-    document.getElementById('minefield').appendChild(blank);
-}
-
-function newNumber(left, top, value) {
-    const numberHint = document.createElement('div')
-    const number = document.createElement('h3');
-    number.innerHTML = value;
-    numberHint.classList.add('space', 'number');
-    numberHint.style.left = ((left * 10) + 5) + '%';
-    numberHint.style.top = ((top * 10) + 5) + '%';
-    numberHint.appendChild(number);
-    numberHint.id = 'cave:' + left + '/' + top;
-    document.getElementById('minefield').appendChild(numberHint);
-};
-
-function distributeFloor() {
-    for (let x = 0; x < 10; x++) {
-        for (let y = 0; y < 10; y++) {
-            newFloor(x, y);
+function toogleFlag(element) {
+    if (element.classList.contains('floor')) {
+        if (element.innerHTML == 'F') {
+            element.innerHTML = '';
+            element.style.color = 'black';
+            element.onclick = function() { showElement(element) };
+        } else {
+            element.innerHTML = 'F';
+            element.style.color = 'blue';
+            element.onclick = function() {};
         }
     }
 }
@@ -123,45 +92,5 @@ function numberOfAdjacentBombs(x, y) {
     return number;
 }
 
-function plantBombs() {
-    for (let x = 0; x < 10; x++) {
-        for (let y = 0; y < 10; y++) {
-            if (Math.random() > 0.99) {
-                minesweeper[[x, y]] = 'B';
-                newBomb(x, y);
-            }
-        }
-    }
-}
-
-function setNumbers() {
-    for (let x = 0; x < 10; x++) {
-        for (let y = 0; y < 10; y++) {
-            let number = numberOfAdjacentBombs(x, y);
-            if ((number > 0) && (minesweeper[[x, y]] != 'B')) {
-                minesweeper[[x, y]] = 'N';
-                newNumber(x, y, number);
-            }
-        }
-    }
-}
-
-function setBlanks() {
-    for (let x = 0; x < 10; x++) {
-        for (let y = 0; y < 10; y++) {
-            if ((minesweeper[[x, y]] != 'B') && (minesweeper[[x, y]] != 'N')) {
-                minesweeper[[x, y]] = '0';
-                newBlank(x, y);
-            }
-        }
-    }
-}
-
-
-
-
-plantBombs();
-setNumbers();
-setBlanks();
-distributeFloor();
-
+defineElements(xElements, yElements, bombChances);
+positionElements();
